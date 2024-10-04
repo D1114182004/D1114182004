0d345907e856f373f26de63a5b87e56b69ded0de
D1114182004 <D1114182004@gm.lhu.edu.tw>
2024-10-04 15:46:47 +0800

    no message

---
+++ table_gen.php
@@ -0,0 +1,21 @@
+<?php
+
+$row_num = $_GET['row_num'];
+$col_num = $_GET['col_num'];
+
+
+echo '表格大小'.$row_num.'列,'.$col_num.'行';
+
+echo '<table border="1">';
+
+for ($i = 0; $i < $row_num; $i++) {
+    echo '<tr>';
+    for ($j = 0; $j < $col_num; $j++) {
+        echo '<td>'.($i+1).'列,'.($j+1).'行</td>';
+    }
+    echo '</tr>';
+}
+
+echo '</table>';
+
+?>
