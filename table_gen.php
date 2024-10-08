9d7711d2eaeeb0efeb8725bf97e43e782ddda539
D1114182004 <D1114182004@gm.lhu.edu.tw>
2024-10-08 11:52:39 +0800

    增加了一堆東西

--- table_gen.php
+++ table_gen.php
@@ -2,16 +2,22 @@
 
 $row_num = $_GET['row_num'];
 $col_num = $_GET['col_num'];
+$gender = $_GET['gender'];
+$dept = $_GET['dept'];
+$msg = $_GET['message'];
 
 
-echo '表格大小'.$row_num.'列,'.$col_num.'行';
+echo '表格大小'.$col_num.'行,'.$row_num.'列<br/>';
+echo '填表人性別'.$gender.'<br/>';
+echo '科系'.$dept.'<br/>';
+echo '留言'.$msg.'<br/>';
 
 echo '<table border="1">';
 
-for ($i = 0; $i < $row_num; $i++) {
+for ($i = 0; $i < $col_num; $i++) {
     echo '<tr>';
-    for ($j = 0; $j < $col_num; $j++) {
-        echo '<td>'.($i+1).'列,'.($j+1).'行</td>';
+    for ($j = 0; $j < $row_num; $j++) {
+        echo '<td>'.($i+1).'行,'.($j+1).'列</td>';
     }
     echo '</tr>';
 }
